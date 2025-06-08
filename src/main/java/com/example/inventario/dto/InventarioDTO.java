873b773a0c9c7889ddf4abb5c89685526a2533ff
Javiera silva <jav.silvagonza@gmail.com>
2025-06-07 23:10:06 -0400

    segundo commit

---
+++ src/main/java/com/example/inventario/dto/InventarioDTO.java
@@ -0,0 +1,13 @@
+package com.example.inventario.dto;
+
+import lombok.Data;
+
+@Data
+public class InventarioDTO {
+
+    private Integer id_inventario;
+    private String nombre;
+    private int stock;
+
+ 
+}
--- src/main/java/com/example/inventario/view/InventarioView.java
+++
@@ -1,5 +0,0 @@
-package com.example.inventario.view;
-
-public class InventarioView {
-
-}
